e2701716bc2efa88b481db862faa4115ce85e7e8
Érika de Freitas <erikafreitas47@gmail.com>
2021-12-17 16:47:11 -0300

    inserindo port no server para o heroku

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require('dotenv').config()
 
 const PORT = process.env.PORT || 9090;
 
-app.listen(port, () => {
+app.listen(PORT, () => {
     console.log(`Servidor da API - Caroneiros rodando na porta ${PORT}`);
 })
